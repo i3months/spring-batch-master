3a0f0757634d7bad00e0db370661fce1b075885e
i3months <waysoleward01@naver.com>
2024-06-23 14:44:20 +0900

    first commit

---
+++ src/test/java/io/springbatch/spring_batch_master/SpringBatchMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.spring_batch_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
