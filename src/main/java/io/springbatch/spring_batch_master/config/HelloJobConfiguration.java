7963f3f8fa8977be13aa1c32c84bf10075f4945c
i3months <waysoleward01@naver.com>
2024-06-24 22:38:31 +0900

    batch 5 update

--- src/main/java/io/springbatch/spring_batch_master/config/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/spring_batch_master/config/HelloJobConfiguration.java
@@ -1,19 +1,44 @@
 package io.springbatch.spring_batch_master.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class HelloJobConfiguration {
     
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
+    private final JobRepository jobRepository;
+    private PlatformTransactionManager platformTransactionManager;
 
-    public HelloJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return new JobBuilder("helloJob", jobRepository)
+            .start(step())
+            .build();
     }
 
+    @Bean
+    public Step step() {
+        return new StepBuilder("helloStep", jobRepository)
+            .tasklet((contribution, chunkContext) -> {
+                System.out.println("Hello Step");
+                return RepeatStatus.FINISHED;
+            }, platformTransactionManager)
+            .build();
+                   
+    }
     
 }
