fba3e5fa8d43569adc4d66a83d087486bdeff32f
i3months <waysoleward01@naver.com>
2024-07-16 01:09:30 +0900

    flow state

--- src/main/java/io/springbatch/spring_batch_master/flow/SimpleFlowConfig.java
+++ src/main/java/io/springbatch/spring_batch_master/flow/SimpleFlowConfig.java
@@ -3,20 +3,29 @@ package io.springbatch.spring_batch_master.flow;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class SimpleFlowConfig {
 
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+
     @Bean
-    public Job job(JobRepository jobRepository) {
+    public org.springframework.batch.core.Job job(JobRepository jobRepository) {
         return new JobBuilder("helloJob", jobRepository)
-            .start(flow())
-            .next(step3())
+            .start(step1(jobRepository, platformTransactionManager))
+            .on("COMPLETED").to(step2(jobRepository, platformTransactionManager))
+            .from(step1(jobRepository, platformTransactionManager))            
+            .on("FAILED").to(flow())
             .end()
             .build();
     }
@@ -24,11 +33,12 @@ public class SimpleFlowConfig {
     @Bean
     public Flow flow() {
         FlowBuilder<Flow> builder = new FlowBuilder<>("flow");
-        builder.start(step1()) 
-                .next(step2())
+        builder.start(step2(jobRepository, platformTransactionManager)) 
+                .on("*")
+                .to(step3(jobRepository, platformTransactionManager))
                 .end();
 
-        builder.build();
+        return builder.build();
     }
 
     @Bean
