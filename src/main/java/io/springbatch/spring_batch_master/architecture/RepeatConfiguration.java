dcd66890e396a02e25d4d5fbf1d4b20c28c3f16e
i3months <waysoleward01@naver.com>
2024-08-03 23:55:14 +0900

    repeatStatus

---
+++ src/main/java/io/springbatch/spring_batch_master/architecture/RepeatConfiguration.java
@@ -0,0 +1,12 @@
+package io.springbatch.spring_batch_master.architecture;
+
+import org.springframework.batch.core.Step;
+import org.springframework.context.annotation.Bean;
+
+public class RepeatConfiguration {
+    
+    @Bean
+    public Step step1() throws Exception {
+        
+    }
+}
